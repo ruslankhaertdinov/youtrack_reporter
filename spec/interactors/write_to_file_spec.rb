0d7b18ee7ddb4f2fc882c79ae993c814a3f3939b
Ruslan Khaertdinov <ruslanimos@ya.ru>
2019-01-08 18:10:34 +0300

    Протестировал сохранение файла отчёта

--- spec/interactors/write_to_file_spec.rb
+++ spec/interactors/write_to_file_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 describe WriteToFile do
-  let(:interactor) { described_class.call(parsed_data: parsed_data) }
-  let(:parsed_data) do
-    array = JSON.parse(File.read('spec/fixtures/report.json'))
+  let(:interactor) { described_class.call(grouped_data: grouped_data) }
+  let(:grouped_data) do
+    array = JSON.parse(File.read('spec/fixtures/grouped_report.json'))
     array.map(&:deep_symbolize_keys)
+  end
+  let(:file_path) { 'tmp/report.xlsx' }
+
+  before do
+    File.delete(file_path) if File.exist?(file_path)
+  end
+
+  it 'создаст файл' do
+    expect(File.exist?(file_path)).to eq(false)
+
+    interactor
+
+    expect(File.exist?(file_path)).to eq(true)
   end
 end
