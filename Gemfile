9d121a9634e40aeef62f7ee7844384dc86f20f0b
Ruslan Khaertdinov <ruslanimos@ya.ru>
2019-01-08 15:46:50 +0300

    Добавил парсинг загружаемого файла

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem "simple_form"
 gem "slim"
 
 # all other gems
+gem 'axlsx'
 gem "decent_decoration"
 gem "decent_exposure"
 gem "devise"
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,5 +3,18 @@ class ReportsController < ApplicationController
   end
 
   def create
+    result = CreateReport.call(source_path: report_params[:file].path)
+    if result.success?
+      send_file result.report_path, filename: "Отчёт #{ Date.current }.xlsx"
+    else
+      flash.now[:alert] = result.error
+      render :new
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:file)
   end
 end
